c0dca0db4dd244a8916387d0d38cf6fd089f9195
Evan Owenby <owenby_evan@ad1.columbusstate.edu>
2023-03-16 11:17:50 -0400

    Add project files.

---
+++ Building Plans Construction.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBuildingPlansConversion
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblHeader = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.lblMsg = New System.Windows.Forms.Label()
+        Me.radInToM = New System.Windows.Forms.RadioButton()
+        Me.RadMToIn = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.txtUsrInput = New System.Windows.Forms.TextBox()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Midterm.My.Resources.Resources.building
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(285, 221)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'lblHeader
+        '
+        Me.lblHeader.AutoSize = True
+        Me.lblHeader.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeader.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblHeader.Location = New System.Drawing.Point(436, 23)
+        Me.lblHeader.Name = "lblHeader"
+        Me.lblHeader.Size = New System.Drawing.Size(387, 63)
+        Me.lblHeader.TabIndex = 1
+        Me.lblHeader.Text = "Converter App"
+        '
+        'btnConvert
+        '
+        Me.btnConvert.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnConvert.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConvert.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnConvert.Location = New System.Drawing.Point(103, 484)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(166, 88)
+        Me.btnConvert.TabIndex = 2
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnExit.Location = New System.Drawing.Point(784, 484)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(166, 88)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btnClear.Location = New System.Drawing.Point(447, 484)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(166, 88)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'lblMsg
+        '
+        Me.lblMsg.AutoSize = True
+        Me.lblMsg.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMsg.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblMsg.Location = New System.Drawing.Point(441, 119)
+        Me.lblMsg.Name = "lblMsg"
+        Me.lblMsg.Size = New System.Drawing.Size(241, 62)
+        Me.lblMsg.TabIndex = 5
+        Me.lblMsg.Text = "Enter a value and" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "choose conversion"
+        '
+        'radInToM
+        '
+        Me.radInToM.AutoSize = True
+        Me.radInToM.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.radInToM.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radInToM.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.radInToM.Location = New System.Drawing.Point(524, 274)
+        Me.radInToM.Name = "radInToM"
+        Me.radInToM.Size = New System.Drawing.Size(190, 30)
+        Me.radInToM.TabIndex = 6
+        Me.radInToM.TabStop = True
+        Me.radInToM.Text = "Inches to Meters"
+        Me.radInToM.UseVisualStyleBackColor = False
+        '
+        'RadMToIn
+        '
+        Me.RadMToIn.AutoSize = True
+        Me.RadMToIn.BackColor = System.Drawing.SystemColors.ControlText
+        Me.RadMToIn.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadMToIn.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.RadMToIn.Location = New System.Drawing.Point(524, 310)
+        Me.RadMToIn.Name = "RadMToIn"
+        Me.RadMToIn.Size = New System.Drawing.Size(190, 30)
+        Me.RadMToIn.TabIndex = 7
+        Me.RadMToIn.TabStop = True
+        Me.RadMToIn.Text = "Meters to Inches"
+        Me.RadMToIn.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ControlText
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.Location = New System.Drawing.Point(507, 231)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(282, 31)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Convert Measurement"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.SystemColors.ControlText
+        Me.PictureBox2.Location = New System.Drawing.Point(495, 213)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(360, 155)
+        Me.PictureBox2.TabIndex = 9
+        Me.PictureBox2.TabStop = False
+        '
+        'txtUsrInput
+        '
+        Me.txtUsrInput.BackColor = System.Drawing.SystemColors.ControlText
+        Me.txtUsrInput.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUsrInput.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.txtUsrInput.Location = New System.Drawing.Point(723, 119)
+        Me.txtUsrInput.Name = "txtUsrInput"
+        Me.txtUsrInput.Size = New System.Drawing.Size(132, 68)
+        Me.txtUsrInput.TabIndex = 10
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOutput.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lblOutput.Location = New System.Drawing.Point(489, 408)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(0, 31)
+        Me.lblOutput.TabIndex = 11
+        '
+        'frmBuildingPlansConversion
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1061, 609)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.txtUsrInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadMToIn)
+        Me.Controls.Add(Me.radInToM)
+        Me.Controls.Add(Me.lblMsg)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.lblHeader)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Name = "frmBuildingPlansConversion"
+        Me.Text = "Building Plans Conversion"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents lblMsg As Label
+    Friend WithEvents radInToM As RadioButton
+    Friend WithEvents RadMToIn As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents txtUsrInput As TextBox
+    Friend WithEvents lblOutput As Label
+End Class
---
+++ Building Plans Construction.vb
@@ -0,0 +1,48 @@
+﻿Public Class frmBuildingPlansConversion
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        ' The btnConvert event handler calculates the converted inputted number
+        ' and then displays the result
+
+        Dim decConversionFactor As Decimal = 39.37D
+        Dim decUsrInput As Decimal
+        Dim decOutput As Decimal
+        Dim strFirstWord As String = ""
+        Dim strSecondWord As String = ""
+
+        ' User Input Validation
+        If IsNumeric(txtUsrInput.Text) Then
+            decUsrInput = Convert.ToDecimal(txtUsrInput.Text)
+            If decUsrInput > 0 Then
+                If radInToM.Checked Then
+                    decOutput = decUsrInput / decConversionFactor
+                    strFirstWord = "inches"
+                    strSecondWord = "meters"
+                ElseIf RadMToIn.Checked Then
+                    decOutput = decUsrInput * decConversionFactor
+                    strFirstWord = "meters"
+                    strSecondWord = "inches"
+                End If
+                lblOutput.Text = Convert.ToString(decUsrInput) + " " + strFirstWord + " is " + Convert.ToString(Math.Round(decOutput, 3)) + " " + strSecondWord + "."
+            Else
+                MsgBox("You entered " + decUsrInput.ToString() + ". Please enter a positive number", , "Input Error")
+                txtUsrInput.Text = ""
+                txtUsrInput.Focus()
+            End If
+        Else
+            MsgBox("The text you entered is not a number. Please enter a number", , "Input Error")
+            txtUsrInput.Text = ""
+            txtUsrInput.Focus()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtUsrInput.Text = ""
+        lblOutput.Text = ""
+        radInToM.Checked = False
+        RadMToIn.Checked = False
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
+    End Sub
+End Class
